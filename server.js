785ebb4fc646862da3a5c1a2986d8643ed0deb52
Edaurd Kosyan <edkosyan2000@gmail.com>
2024-12-04 12:09:13 +0400

    peer

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const { createServer } = require("node:http");
 const { Server } = require("socket.io");
-const { join } = require("node:path");
 
 const app = express();
 const server = createServer(app);
+
 const io = new Server(server, {
   cors: {
     origin: "*", // Allow all origins for development (adjust as needed)
@@ -12,63 +12,32 @@ const io = new Server(server, {
   },
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
-});
-
 io.on("connection", (socket) => {
-  //List
-  const arr = Array.from(io.sockets.sockets.keys());
-  io.emit("connection-started", arr);
-   // Handle disconnect
-   socket.on("disconnect", () => {
-    const arr = Array.from(io.sockets.sockets.keys());
-    io.emit("connection-started", arr);
-    console.log(`User disconnected: ${socket.id}`);
+  //Start
+  socket.on("start", (d) => {
+    io.emit("start", d);
   });
-  ///////////////////////////////////////////////
-  ///////////////////////////////////////
-  ///////////////////////////////
-  //Session
-  socket.on("session", (arg) => {
-    //Start
-    if (arg === "start") {
-      io.emit("session", "start");
-    }
-    //Stop
-    if (arg === "stop") {
-      io.emit("session", "stop");
-    }
+  //Stop
+  socket.on("stop", (d) => {
+    io.emit("stop", d);
   });
   //Settings
   socket.on("settings", (d) => {
     io.emit("settings", d);
   });
-  //Connect mobile device
-  socket.on("device", (d) => {
-    io.emit("device", d);
-  });
   //Signal
   socket.on("signal", (d) => {
     socket.broadcast.emit("signal", d);
   });
-  // WebRTC below
-  socket.on("offer", (data) => {
-    console.log("Offer received:", data);
-    socket.broadcast.emit("offer", data); // Send offer to other peer
+  //WebRTC bellow
+  const arr = Array.from(io.sockets.sockets.keys());
+  io.emit("connection-started", arr);
+  // Handle disconnect
+  socket.on("disconnect", () => {
+    const arr = Array.from(io.sockets.sockets.keys());
+    io.emit("connection-started", arr);
+    console.log(`User disconnected: ${socket.id}`);
   });
-
-  socket.on("answer", (data) => {
-    console.log("Answer received:", data);
-    socket.broadcast.emit("answer", data); // Send answer to other peer
-  });
-
-  socket.on("ice-candidate", (data) => {
-    console.log("ICE Candidate received:", data);
-    socket.broadcast.emit("ice-candidate", data); // Send ICE candidate to other peer
-  });
-
- 
 });
 
 server.listen(3000, () => {
